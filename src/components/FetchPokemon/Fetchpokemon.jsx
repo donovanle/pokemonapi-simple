f5a523db5e18812ef740e1c5c5e70da6f3ecfd64
donovanlem <kiloatw@gmail.com>
2022-03-15 17:12:16 -0600

    pokemon api using axios

--- src/components/FetchPokemon/Fetchpokemon.jsx
+++ src/components/FetchPokemon/Fetchpokemon.jsx
@@ -1,19 +1,18 @@
 import React,{useEffect, useState} from 'react'
-
+import axios from 'axios';
 const Fetchpokemon = () => {
-    const [pokemon, setPokemon] = useState([])
+    const [pokemons, setPokemon] = useState([])
 
-    const fetchPokemon = async() =>{
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=803")
-        console.log(response)
-        const jsonResponse = await response.json()
-        setPokemon(jsonResponse.results)   
+    const fetchAllPokemon = async() =>{
+        const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+        console.log(response.data.results)
+        setPokemon(response.data.results)   
     };
 
   return (
     <div>
-        <button onClick={fetchPokemon}>Fetch Pokemon</button>
-        {pokemon? pokemon.map((poke, index)=>{
+        <button onClick={fetchAllPokemon}>Fetch Pokemon</button>
+        {pokemons? pokemons.map((poke, index)=>{
             return (<p key={index}>{poke.name}</p>)
         }):<p>Click Button To Fetch Pokemon</p>}
     </div>
