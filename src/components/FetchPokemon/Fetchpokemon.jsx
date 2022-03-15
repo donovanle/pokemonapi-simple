f69adb52a067cee720d0085ff1250f2794ed817a
donovanlem <kiloatw@gmail.com>
2022-03-15 16:00:29 -0600

    pokemon api simple

--- src/components/FetchPokemon/Fetchpokemon.jsx
+++ src/components/FetchPokemon/Fetchpokemon.jsx
@@ -6,13 +6,13 @@ const Fetchpokemon = () => {
     const fetchPokemon = () =>{
         fetch("https://pokeapi.co/api/v2/pokemon?limit=803")
             .then(response => {return response.json()})
-            .then(response => {setPokemon({data:response.results})})    
+            .then(response => {setPokemon(response.results)})    
     };
 
   return (
     <div>
         <button onClick={fetchPokemon}>Fetch Pokemon</button>
-        {pokemon.data? pokemon.data.map((poke, index)=>{
+        {pokemon? pokemon.map((poke, index)=>{
             return (<p key={index}>{poke.name}</p>)
         }):<p>Click Button To Fetch Pokemon</p>}
     </div>
